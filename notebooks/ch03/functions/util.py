7841a243a473a53af34af7e917af207c62238e30
Jim Dowling <jim@hopsworks.ai>
2024-06-12 00:10:16 +0200

    monitoring fixes

--- notebooks/ch03/functions/util.py
+++ notebooks/ch03/functions/util.py
@@ -284,3 +284,18 @@ def check_file_path(file_path):
         print(f"Error. File not found at the path: {file_path} ")
     else:
         print(f"File successfully found at the path: {file_path}")
+
+def backfill_predictions_for_monitoring(weather_fg, air_quality_df, monitor_fg, model):
+    features_df = weather_fg.read()
+    features_df = features_df.sort_values(by=['date'], ascending=True)
+    features_df = features_df.tail(10)
+    features_df['predicted_pm25'] = model.predict(features_df[['temperature_2m_mean', 'precipitation_sum', 'wind_speed_10m_max', 'wind_direction_10m_dominant']])
+    df = pd.merge(features_df, air_quality_df[['date','pm25','street','country']], on="date")
+    df['days_before_forecast_day'] = 1
+    hindcast_df = df
+    df = df.drop('pm25', axis=1)
+    monitor_fg.insert(df, write_options={"wait_for_job": True})
+    return hindcast_df
+    
+    
+    
